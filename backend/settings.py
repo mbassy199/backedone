b6dfa8ee46587213dfa2f71025168cc351efa3ac
mbassy199 <awuleynovember@gmail.com>
2025-01-17 13:08:52 +0000

    update

--- backend/settings.py
+++ backend/settings.py
@@ -36,17 +36,7 @@ DEBUG = True
 
 
 
-ALLOWED_HOSTS = [
-    "backend-priti-production.up.railway.app",  # Correct Railway backend domain
-    "my.mypritistore.com",                      # Custom domain with 'www'
-    ".railway.app",                             # All Railway subdomains
-    "ecopritistore.com",                        # Custom domain
-    "ecommerce-frontend-two-ruddy.vercel.app",  # Vercel frontend domain
-    "backedone-89yvv0ir0-williams-projects-81f89765.vercel.app",  # Vercel backend domain
-    "backedone-f894aorwy-williams-projects-81f89765.vercel.app",  # New Vercel backend domain
-    "127.0.0.1",                                # Localhost
-    "localhost",                                # Localhost with no port
-]
+ALLOWED_HOSTS = ['*']
 
 
 CSRF_TRUSTED_ORIGINS = [
